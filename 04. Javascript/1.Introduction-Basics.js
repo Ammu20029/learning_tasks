99697903e6191d1064656ee0a7c3cd608e83c1e4
Ammu20029 <prasannatalada748@gmail.com>
2025-09-17 15:52:42 +0530

    add these files

---
+++ 04. Javascript/1.Introduction-Basics.js
@@ -0,0 +1,24 @@
+let s="String1";
+const v=5;
+let x=10;
+let y;
+let z=null;
+let a=new Array(20,10,50);
+console.log("value of x:"+x);// value of x
+console.log("value of s:"+s);
+console.log("value of y:"+y);
+console.log("value of z:"+z);
+console.log("value of a:"+a);
+console.log("addition"+(x+s));// addition of number and string
+console.log("substraction"+(x-v));
+console.log("multiplication"+(x*v));
+console.log("division"+(x/v));/* division of 2 numbers */
+console.log("Modulus"+(x%v));
+console.log("increment"+(x++));
+console.log("decrement"+(x--));/* decrement operator */
+console.log("logical and:"+(3&&5));
+console.log("logical or:"+(x||v));
+console.log("greater than:"+(x>v));
+console.log("less than:"+(v<x));
+console.log("assignment operator:"+(x*=v)); // x=x*v
+console.log("assignment operator:"+(x/=y)); //x=x/y
