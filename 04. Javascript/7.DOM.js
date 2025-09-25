67cceb0fb93bbc4c53f51c6c897d190e1e265d5c
Ammu20029 <prasannatalada748@gmail.com>
2025-09-25 12:43:54 +0530

    style/feature

--- 04. Javascript/7.DOM.js
+++ 04. Javascript/7.DOM.js
@@ -2,24 +2,24 @@ const element=document.getElementById("abc");
 element.style.color="green";
 element.style.textAlign="center";
 element.style.margin="30px";
-element.style.fontsize="30px";
+element.style.fontsize="20px";
 
 const elements=document.getElementsByClassName('selector');
 elements[0].style.color="green";
 elements[1].style.color='red';
 elements[0].style.textAlign='center';
 elements[1].style.textAlign='center';
-elements[0].style.marginTop="60px";
+elements[0].style.marginTop="20px";
 
 const element1=document.querySelector('.efg');
 element1.style.color="green";
 element1.style.textAlign="center";
-element1.style.margin="30px";
+element1.style.margin="20px";
 
 const elements2=document.querySelectorAll('.selector');
 elements2[0].style.color="yellow";
 elements2[0].style.textAlign="center";
-elements2[0].style.marginTop="60px";
+elements2[0].style.marginTop="20px";
 
 document.getElementById("p1").innerHTML="Welcome";
 element.style.backgroundColor="#dbe4e4ff";
@@ -34,4 +34,4 @@ const elmnt=document.getElementById("p3");elmnt.remove();
 
 document.getElementById("p2").style.color="blue";
 document.getElementById("p2").style.fontFamily="Arial";
-document.getElementById("p2").style.fontSize="larger";
+document.getElementById("p2").style.fontSize="medium";
