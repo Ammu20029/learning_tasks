cf18e8a67e199f9d0b137d404b74a9b2f0df7326
Ammu20029 <prasannatalada748@gmail.com>
2025-10-17 14:17:28 +0530

    ES6-concepts

--- 04. Javascript/19.Functions.js
+++ 04. Javascript/19.Functions.js
@@ -28,4 +28,59 @@ bar.call("hello");
 //bind
 const g=getThis.bind({name:"annaballe"});
 console.log(g());
+//promise
+const promise1=Promise.resolve(3);
+const promise2=52;
+const promise3=new Promise((resolve,reject)=>{
+    setTimeout(resolve,100,"hello");
+});
+Promise.all([promise1,promise2,promise3]).then((values)=>{
+    console.log(values);
+});
+const p3=[promise1,promise2];
+Promise.allSettled(promises).then((results)=>
+results.forEach((result)=>
+console.log(result.status)),);
+function loginUser(uname,password){
+    return new Promise((resolve,reject)=>{
+        console.log("logging in");
+        setTimeout(()=>{
+            if(uname==="admin" && password==="4321"){
+                resolve({userId:1,name:"Admin"});
+        
+            }else {
+                reject("Invalid details");
+            }
+        },5000);
+    });
+}
 
+function UserTasks(userId){
+    return new Promise((resolve,reject)=>{
+        console.log("Getting the tasks");
+        setTimeout(()=>{
+            if(userId===1){
+                resolve([
+                    {title:"fix the bug"},,
+                    {title:"update the client"},
+                ]);
+            }else{
+                reject("No tasks were found");
+            }
+    },1000);
+});
+}
+loginUser("admin","4321")
+.then(user=>{
+    console.log(`Welcome, ${user.name}`);
+    return UserTasks(user.userId);
+})
+.then(tasks=>{
+    consople.log("Your tasks:");
+    tasks.ForEach(tasks=>{
+        console.log(`.${tasks.title}`);
+    });
+})
+.catch(error=>{
+    console.error();
+});
